d4690cc906260596a46363ca8c810d23bc020f89
Ahmet0z <l9120016@std.yildiz.edu.tr>
2021-08-04 13:24:49 +0300

    8.Ders Sonu

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic Constraint
+    //class : referans tip olabilir
+    //new() : new'lenebilir olmalı
+    //T bir referans tip, IEntity veya IEntity implement eden bir nesne ve new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T Get(Expression<Func<T, bool>> filter = null);
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+    }
+}
