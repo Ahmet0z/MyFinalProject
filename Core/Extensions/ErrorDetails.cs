63671531232d4e43d7f21ab1ca8fa431345b9cea
Ahmet0z <l9120016@std.yildiz.edu.tr>
2021-10-30 13:22:38 +0300

    20.Ders Sonu

---
+++ Core/Extensions/ErrorDetails.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Newtonsoft.Json;
+
+namespace Core.Extensions
+{
+    public class ErrorDetails
+    {
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
